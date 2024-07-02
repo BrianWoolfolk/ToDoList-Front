3db98befb9481c0826f50dea58c3b9664859719a
Brian Woolfolk <brian.woolfolk@encora.com>
2024-07-02 08:57:31 -0700

    disable on loading

--- src/components/TodoModal.jsx
+++ src/components/TodoModal.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useSubmit } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigation, useSubmit } from "react-router-dom";
 import Input from "./Input";
 import { useRefresh } from "../scripts/scripts";
 
@@ -31,22 +31,30 @@ const TodoModal = (props) => {
   const [refresh, volkey] = useRefresh();
   const submit = useSubmit();
   const [open, setOpen] = useState(!!props.edit);
+  const [loading, setLoading] = useState(false);
+  const navi = useNavigation();
 
   function handleCreate() {
+    if (loading) return;
     setLS(newToDo());
     setOpen(true);
     refresh();
   }
 
-  function handleCancel() {
-    setLS({});
-    setOpen(false);
+  const handleCancel = useCallback(
+    (bypass) => {
+      if (loading && !bypass) return;
+      setLS({});
+      setOpen(false);
 
-    if (props.edit) props.onClose?.();
-  }
+      if (props.edit) props.onClose?.();
+    },
+    [loading, props]
+  );
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (loading) return;
 
     const data = new FormData();
     data.append("text", LS.text);
@@ -59,16 +67,22 @@ const TodoModal = (props) => {
     data.append("priority", LS.priority);
     data.append("id", LS.id);
 
+    setLoading(true);
     submit(data, { method: props.edit ? "PUT" : "POST" });
   }
 
+  useEffect(() => {
+    if (navi.state === "idle" && loading) {
+      setLoading(false);
+      handleCancel(true);
+    }
+  }, [loading, navi.state, handleCancel]);
+
   return (
     <>
-      {
-        <button disabled={props.edit} onClick={handleCreate}>
-          + New To Do
-        </button>
-      }
+      <button disabled={props.edit || open || loading} onClick={handleCreate}>
+        {loading ? "Loading..." : "+ New To Do"}
+      </button>
 
       {open && (
         <form onSubmit={handleSubmit} key={volkey}>
@@ -78,12 +92,14 @@ const TodoModal = (props) => {
             _type={"text"}
             _required
             _label={"Text message"}
+            _disabled={loading}
           />
           <Input
             _store={LS}
             _store_var={"due_date"}
             _type={"date"}
             _label={"Due date (optional)"}
+            _disabled={loading}
           />
           <Input
             _store={LS}
@@ -91,11 +107,14 @@ const TodoModal = (props) => {
             _select_from={["LOW", "MEDIUM", "HIGH"]}
             _required
             _label={"Priority"}
+            _disabled={loading}
           />
 
-          <button>Save Item</button>
+          <button disabled={loading}>
+            {loading ? "Loading..." : "Save Item"}
+          </button>
 
-          <button type={"button"} onClick={handleCancel}>
+          <button type={"button"} disabled={loading} onClick={handleCancel}>
             Cancel
           </button>
         </form>
